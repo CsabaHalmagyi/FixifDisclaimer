07b54f5699d1470a3fc5fdd95c7330b90334e4dd
Artenides <artenides@gmail.com>
2020-02-09 14:25:34 +0000

    Define form fields

---
+++ src/uk/co/fixif/Form.java
@@ -0,0 +1,22 @@
+
+
+public class Form{
+
+
+
+    private String jobDetails;
+
+    private String dateOfJob;
+    private String signature;
+
+    private String submitButton;
+
+    private String emailAddress; // the email used to send the data to
+
+    private String smtpSettings; // sender email settings
+
+
+
+
+
+}
